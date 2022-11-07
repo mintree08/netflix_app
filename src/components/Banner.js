2acd63e6013c85e280a8e00e951177f00f090f1a
mintree08 <dakyung0829@gmail.com>
2022-11-07 11:19:10 +0900

    first commit

---
+++ src/components/Banner.js
@@ -0,0 +1,102 @@
+import axios from '../api/axios';
+import React, { useEffect, useState } from 'react'
+import requests from '../api/requests'
+import '../styles/Banner.css'
+import styled from 'styled-components';
+
+function Banner() {
+    const [movie, setMovie] = useState([]);
+    const [isClicked, setIsClicked] = useState([false]);
+
+
+    useEffect(() => {
+        fetchData();
+    },[]);
+
+    const fetchData = async () => {
+        // 현재 상영중인 영화 정보 가져오기(20영화)
+       const request = await axios.get(requests.fetchNowPlaying);
+       //console.log(request);
+
+       //20개 영화중 영화 하나의 ID 가져오기
+       const movieId = request.data.results[
+      Math.floor(Math.random() * request.data.results.length + 0) 
+       ].id;
+       //특정 영화의 더 상세한 정보 가져오기(videos 정보도 포함)
+       const {data:movieDetail} = await axios.get(`movie/${movieId}` ,{
+        params:{append_to_response: "videos"}
+    });
+        console.log(movieDetail);
+        setMovie(movieDetail);
+    }
+
+    const truncate = (str, n) => {
+        return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+    }
+ if(!isClicked){
+  return (
+    <header className="banner"
+        style={{
+        backgroundImage:`url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
+        backgroundPosition: "top center",
+        backgroundSize: "cover",
+        }}>
+
+        <div className="banner__contents">
+        <h1 className="banner__title">
+            {movie.title || movie.name || movie.original_name}
+        </h1>
+        <div className="banner_buttons">
+            <button className="banner_button play"
+            onClick={() => setIsClicked(true)}>Play</button>
+            <button  className="banner_button info">More Information</button>
+        </div>
+        <p className="banner__description">
+            {movie.overview}
+        </p>
+        </div>
+        <div className="banner__fadeBottom">
+            {truncate(movie.overview,100)}
+        </div>
+    </header>
+  )
+}else{
+    return (
+      <Container>
+        <HomeContainer>
+            <Iframe src={`https://www.youtube.com/embed/${movie.videos.results[0].key}?
+            controls=0&autoplay=1&loop=1&mute=1&playlist=${movie.videos.results[0].key}`} 
+            width="640" height="360" frameborder="0" allow="autoplay; fullscreen"
+            title="YouTube video player" allowFullScreen
+            ></Iframe>
+        </HomeContainer>
+      </Container>
+    )
+}
+}
+
+const Container = styled.div`
+display: flex; 
+flex-direction: column; 
+justify-content: center; 
+align-items: center; 
+width: 100%; 
+height: 100vh;
+`;
+const HomeContainer = styled.div`
+width: 100%;
+height: 100%;
+`;
+
+const Iframe = styled.iframe`
+width: 100%; 
+height: 100%; 
+z-index: -1; 
+opacity: 0.65; 
+border: none; 
+&::after{
+    content: ""; position: absolute; 
+    top: 0; left: 0; width: 100%; height: 100%;}
+    
+`;
+export default Banner
---
+++ src/hooks/useDebounce.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function useDebounce() {
+  return (
+    <div>useDebounce</div>
+  )
+}
+
+export default useDebounce
